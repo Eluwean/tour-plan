7758538a83197e80165111009273518caf514474
Eluwean <d.stayn@mail.ru>
2021-05-25 18:57:45 +0300

    try-one

--- js/main.js
+++ js/main.js
@@ -1,34 +1,11 @@
-<<<<<<< HEAD
-var hotelSLider = new Swiper(".hotel-slider", {
-=======
 const swiper = new Swiper(".swiper-container", {
->>>>>>> 78a0c4dc2164b31fb287d21ec24e28ef9f8091d7
 	// Optional parameters
 	loop: true,
 
 	// Navigation arrows
 	navigation: {
-<<<<<<< HEAD
-		nextEl: ".hotel-slider__button--next",
-		prevEl: ".hotel-slider__button--prev",
-	},
-	effect: "cube",
-});
-
-var reviewsSlider = new Swiper(".reviews-slider", {
-	// Optional parameters
-	loop: true,
-
-	// Navigation arrows
-	navigation: {
-		nextEl: ".reviews-slider__button--next",
-		prevEl: ".reviews-slider__button--prev",
-	},
-});
-=======
 		nextEl: ".slider-button--next",
 		prevEl: ".slider-button--prev",
 	},
 	effect: "cube",
 });
->>>>>>> 78a0c4dc2164b31fb287d21ec24e28ef9f8091d7
